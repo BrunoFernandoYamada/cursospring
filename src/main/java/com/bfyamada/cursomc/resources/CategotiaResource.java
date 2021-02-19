4ae3544ace93ddbad459e162728f5ce756df204d
Bruno Yamada <yamada_bruno@hotmail.com>
2021-02-19 18:18:40 -0300

    REST testado

---
+++ src/main/java/com/bfyamada/cursomc/resources/CategotiaResource.java
@@ -0,0 +1,16 @@
+package com.bfyamada.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategotiaResource {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando";
+	}
+
+}
