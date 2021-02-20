a1da117129616c9246efb4da5360f2964c96fbdf
Bruno Yamada <yamada_bruno@hotmail.com>
2021-02-19 21:29:26 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/bfyamada/cursomc/resources/handlers/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.bfyamada.cursomc.resources.handlers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.bfyamada.cursomc.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), ex.getMessage(),System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+
+}
