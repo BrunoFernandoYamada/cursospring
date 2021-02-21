e3e0c696da1f7872bf601e3dc732e61d263341c8
Bruno Yamada <yamada_bruno@hotmail.com>
2021-02-21 17:27:54 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/bfyamada/cursomc/services/CategoriaService.java
+++ src/main/java/com/bfyamada/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.bfyamada.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possíval excluir uma Categoria que possui produtos.");
 		}
 
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
